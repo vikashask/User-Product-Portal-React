b43185220335c411b0f5976a32a3ddb61db105d7
vikash <vikashraj144@gmail.com>
2019-07-28 16:33:04 +0530

    server added

---
+++ server/server.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let app = express();
+let mongoose = require('mongoose');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+let port = 8080;
+let user = require('./app/routes/user');
+
+let config = require('config'); //we load the db location from the JSON files
+//db options
+let options = {
+    server: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000
+        }
+    },
+    replset: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000
+        }
+    }
+};
+
+//db connection      
+mongoose.connect(config.DBHost, options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//don't show the log when it is test
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+    //use morgan to log at command line
+    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
+
+app.listen(port);
+console.log("Listening on port " + port);
+
+module.exports = app; // for testing
