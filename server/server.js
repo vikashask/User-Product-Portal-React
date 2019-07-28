b23898267f89c37aa6e3ca0d4f0786b8cb281887
vikash <vikashraj144@gmail.com>
2019-07-28 16:48:48 +0530

    route added in server js

--- server/server.js
+++ server/server.js
@@ -34,6 +34,45 @@ if (config.util.getEnv('NODE_ENV') !== 'test') {
     app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
 }
 
+// adding allow access controll
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+//parse application/json and look for raw text                                        
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: 'application/json'
+}));
+
+// route
+app.get("/", (req, res) => res.json({
+    message: "Welcome to our Demo"
+}));
+
+app.route("/user")
+    .get(user.getUsers)
+    .post(user.postUser);
+
+app.route("/login")
+    .post(user.login);
+
+app.route("/register")
+    .post(user.register);
+
 app.listen(port);
 console.log("Listening on port " + port);
 
