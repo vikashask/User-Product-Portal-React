42d82982698a8fbaf39738d93b4cd560ccfcca85
vikash <vikashraj144@gmail.com>
2019-07-30 16:21:28 +0530

    removed  option because deperecated

--- server/server.js
+++ server/server.js
@@ -7,24 +7,9 @@ let port = 8080;
 let user = require('./app/routes/user');
 
 let config = require('config'); //we load the db location from the JSON files
-//db options
-let options = {
-    server: {
-        socketOptions: {
-            keepAlive: 1,
-            connectTimeoutMS: 30000
-        }
-    },
-    replset: {
-        socketOptions: {
-            keepAlive: 1,
-            connectTimeoutMS: 30000
-        }
-    }
-};
 
 //db connection      
-mongoose.connect(config.DBHost, options);
+mongoose.connect(config.DBHost);
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
