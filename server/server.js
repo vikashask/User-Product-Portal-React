0df09971268790b0193ffe947e64d22e45685d50
vikash <vikashraj144@gmail.com>
2019-07-31 00:28:25 +0530

    cores enabled

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ if (config.util.getEnv('NODE_ENV') !== 'test') {
 
 // adding allow access controll
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
