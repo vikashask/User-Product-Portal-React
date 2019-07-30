86fa1f3b3622bd37a9160654dc1668735b324c0f
vikash <vikashraj144@gmail.com>
2019-07-30 17:33:37 +0530

    user by id added

--- server/app/routes/user.js
+++ server/app/routes/user.js
@@ -1,84 +1,61 @@
-let mongoose = require('mongoose');
 let User = require('../models/user');
-
 /*
  * GET /User route to retrieve all the Users.
  */
 function getUsers(req, res) {
-    //Query the DB and if no errors, send all the Users
-    let query = User.find({});
-    query.exec((err, Users) => {
-        if (err) res.send(err);
-        //If no errors, send them back to the client
-        res.json(Users);
-    });
+    try {
+        //Query the DB and if no errors, send all the Users
+        let query = User.find({});
+        query.exec((err, Users) => {
+            if (err) res.send(err);
+            //If no errors, send them back to the client
+            res.json(Users);
+        });
+    } catch (e) {
+        res.send({
+            error: e,
+        });
+    }
+
 }
 
-function postUser(req, res) {
-    // create new user
-    let newUser = new User(req.body);
-    // save it into DB
-    newUser.save(
-        (err, user) => {
-            if (err) {
-                res.send(err);
-            } else {
+let userbyid = async (req, res) => {
+    try {
+        console.log("req.params.id", req.params);
+
+        if (req.params.id) {
+            let userData = await User.findOne({
+                _id: req.params.id,
+            }, {
+                password: 0
+            });
+            if (userData) {
                 res.send({
-                    message: "User addd!",
-                    user
+                    data: userData,
+                });
+            }
+        } else {
+            let userData = await User.find();
+            if (userData) {
+                res.send({
+                    data: userData,
                 });
             }
         }
-    );
-}
-
-function login(req, res) {
-    console.log(req.body, 'login data---');
-    User.find({
-        email: req.body.email,
-        password: req.body.password
-    }, (function (error, data) {
-        res.json(data);
-    }));
-}
-
-let register = async (req, res) => {
-    try{
-    let userData = await User.findOne({
-        email: req.body.email,
-        password: req.body.password
-    });
-    if (userData) {
+    } catch (e) {
+        console.log("error", e);
         res.send({
-            message: "Email is exist! try with another email",
+            error: e,
         });
-    } else {
-        // create new user
-        let newUser = new User(req.body);
-        // saveing heres
-        newUser.save(
-            (err, user) => {
-                if (err) {
-                    res.send(err);
-                } else {
-                    res.send({
-                        message: "User addd!",
-                        user
-                    });
-                }
-            }
-        );
     }
-}catch(e){
-    console.log("error",e);
-    
-}
-
 }
 
 module.exports = {
     getUsers,
     login,
     register,
-    postUser
+    postUser,
+    editUser,
+    deleteUser,
+    userbyid
 };
