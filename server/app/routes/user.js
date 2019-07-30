e0908ef279f0ec768c9235de84158eea1721a51d
vikash <vikashraj144@gmail.com>
2019-07-30 16:21:59 +0530

    login and register updated

--- server/app/routes/user.js
+++ server/app/routes/user.js
@@ -33,28 +33,47 @@ function postUser(req, res) {
 }
 
 function login(req, res) {
-console.log(req.body,'login data---');
-    User.find({email:req.body.email,password:req.body.password},(function(error,data){
+    console.log(req.body, 'login data---');
+    User.find({
+        email: req.body.email,
+        password: req.body.password
+    }, (function (error, data) {
         res.json(data);
     }));
 }
 
-function register(req, res) {
-    // create new user
-    let newUser = new User(req.body);
-    // save it into DB
-    newUser.save(
-        (err, user) => {
-            if (err) {
-                res.send(err);
-            } else {
-                res.send({
-                    message: "User addd!",
-                    user
-                });
+let register = async (req, res) => {
+    try{
+    let userData = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    if (userData) {
+        res.send({
+            message: "Email is exist! try with another email",
+        });
+    } else {
+        // create new user
+        let newUser = new User(req.body);
+        // saveing heres
+        newUser.save(
+            (err, user) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send({
+                        message: "User addd!",
+                        user
+                    });
+                }
             }
-        }
-    );
+        );
+    }
+}catch(e){
+    console.log("error",e);
+    
+}
+
 }
 
 module.exports = {
