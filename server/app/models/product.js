662197a4d4292134f8820d34a3db17910f14fb6e
vikash <vikashraj144@gmail.com>
2019-07-30 18:03:24 +0530

    product model added

---
+++ server/app/models/product.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+
+let ProductSchema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+}, {
+    versionKey: false
+});
+
+module.exports = mongoose.model('product', ProductSchema);
