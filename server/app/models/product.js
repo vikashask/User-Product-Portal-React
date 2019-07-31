b42f88083830e6f71c250719574b4a4c22c77e27
vikash <vikashraj144@gmail.com>
2019-08-01 00:06:41 +0530

    product schema change

--- server/app/models/product.js
+++ server/app/models/product.js
@@ -5,6 +5,7 @@ let Schema = mongoose.Schema;
 let ProductSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
+    created_by: { type: String, required: true },
     price: { type: Number, required: true },
 }, {
     versionKey: false
