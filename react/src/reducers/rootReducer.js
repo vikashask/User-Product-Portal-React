9d3142aef099c87060d2e6db312c1099ebf9ed4b
vikash <vikashraj144@gmail.com>
2019-08-02 21:31:16 +0530

    allProductReducer added

--- react/src/reducers/rootReducer.js
+++ react/src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import allDataReducer from './allDataReducer';
+import allProductReducer from './allProductReducer';
 
 const rootReducer = combineReducers({
     allData:        allDataReducer,
+    allProductData:        allProductReducer,
 });
 
 export default rootReducer;
