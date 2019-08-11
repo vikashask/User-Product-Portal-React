59f09fe01a2844e51926dd96660fb4c565c1a50c
vikash <vikashraj144@gmail.com>
2019-08-11 20:30:35 +0530

    action type added

--- react/src/actions/actionTypes.js
+++ react/src/actions/actionTypes.js
@@ -1,4 +1,5 @@
 //example
 export const GET_ALL_DATA = "GET_ALL_DATA";
 export const GET_ALL_PRODUCT = "GET_ALL_PRODUCT";
+export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const GET_ALL_USER = "GET_ALL_USER";
