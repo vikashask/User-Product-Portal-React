2dc2c990e933885a63c3627039d082ed37722178
vikash <vikashraj144@gmail.com>
2019-08-11 20:43:17 +0530

    DELETE_PRODUCT reducer

--- react/src/reducers/allProductReducer.js
+++ react/src/reducers/allProductReducer.js
@@ -1,5 +1,7 @@
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
+import * as Constants from "./../utils/Constants";
+import client from "./../utils/restClient";
 
 export default function allProductReducer(state = initialState.allProduct, action) {
 
@@ -7,6 +9,21 @@ export default function allProductReducer(state = initialState.allProduct, actio
         case types.GET_ALL_PRODUCT:
         //Object.assign() is added to aviod redux state mutation
         return  action.allProduct;
+
+        case types.DELETE_PRODUCT:
+                let body = { '_id': action.productId};
+                client.delete(body)
+                .then(data => {
+                    if(data.success=== true){
+                        console.log('final call data',data);
+                        state.productList.splice(action.payload.index, 1)
+                        return  state.productList;
+                    }else{
+                        console.log("deleteRes 2",data);
+                        return state
+                    }
+                }
+            ); 
         default:
             return state;
     }
