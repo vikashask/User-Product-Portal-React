5489e58908e4bc676e131353f8218610f11f74d2
vikash <vikashraj144@gmail.com>
2019-08-12 21:39:41 +0530

    rest client updated

--- react/src/utils/restClient.js
+++ react/src/utils/restClient.js
@@ -2,36 +2,62 @@ import * as Constants from "./../utils/Constants";
 
 export default {
 
-    get : () =>{
-
-    },
-
-    post : () =>{
-
-    },
-
-    put : () =>{
-
-    },
-
-    delete :  async (body) => {
-        console.log("----body from rest client",body);
-        
-        let response = await fetch(Constants.baseURL + 'product',
-        {
-            method: `DELETE`,
+    get: async (apiUrl, param) => {
+        let response = await fetch(Constants.baseURL + 'product', {
+            method: `GET`,
             credentials: `include`,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(body)
+            headers: {
+                'Content-Type': 'application/json',
+            }
         });
         var resObj;
-        if(response.status ===200){
-            resObj = {response:response,success:true};
+        if (response.status === 200) {
+            resObj = {
+                response: response,
+                success: true
+            };
             return await resObj;
-        }else{
-            resObj = {response:response,success:false};
+        } else {
+            resObj = {
+                response: response,
+                success: false
+            };
+            return await resObj;
+        }
+
+    },
+
+    post: () => {
+
+    },
+
+    put: () => {
+
+    },
+
+    delete: async (body) => {
+        console.log("----body from rest client", body);
+
+        let response = await fetch(Constants.baseURL + 'product', {
+            method: `DELETE`,
+            credentials: `include`,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        });
+        var resObj;
+        if (response.status === 200) {
+            resObj = {
+                response: response,
+                success: true
+            };
+            return await resObj;
+        } else {
+            resObj = {
+                response: response,
+                success: false
+            };
             return await resObj;
         }
     }
