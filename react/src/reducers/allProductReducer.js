9c0706089de62ad51e8745b21ec74fd79b83bf3c
vikash <vikashraj144@gmail.com>
2019-08-14 22:01:42 +0530

    DELETE_PRODUCT reducer

--- react/src/reducers/allProductReducer.js
+++ react/src/reducers/allProductReducer.js
@@ -1,33 +1,24 @@
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
 import * as Constants from "./../utils/Constants";
-import client,{getOperation} from "./../utils/restClient";
+import client, {
+    getOperation
+} from "./../utils/restClient";
 
 export default function allProductReducer(state = initialState.allProduct, action) {
 
-    switch(action.type) {
+    switch (action.type) {
         case types.GET_ALL_PRODUCT:
-        return {...state,allProduct:action.payload};
-        break;
+            return {
+                ...state, allProduct: action.payload
+            };
+            break;
 
         case types.DELETE_PRODUCT:
-                let body = { '_id': action.productId};
-                console.log("---action",action);
-                if( action.productId){
-                client.delete({ '_id': action.payload.productId})
-                .then(data => {
-                    if(data.success=== true){
-                        console.log('final call data',data);
-                        state.productList.splice(action.payload.index, 1)
-                        return  state.productList;
-                    }else{
-                        console.log("deleteRes 2",data);
-                        return state
-                    }
-                }
-            ); 
-                }
-                break;
+            return {
+                ...state, delete: action.payload
+            }
+            break;
         default:
             return state;
     }
