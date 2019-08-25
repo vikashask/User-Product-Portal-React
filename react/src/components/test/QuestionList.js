868d502d0c9649bb092008f2c7f13795c6dbbf8d
vikash <vikashraj144@gmail.com>
2019-08-25 22:55:37 +0530

    added question list comp

---
+++ react/src/components/test/QuestionList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as Constants from '../../utils/Constants';
+
+class EachQuestion extends React.Component { 
+      handleChange(e, data){
+        const questionId = data._id;
+        console.log('dafa', e.target.value)
+
+        this.props.handleChange({ [questionId]: e.target.value});
+      }
+
+      render() {
+        // const questionList = this.state.question.map(data=>{
+        // })
+          const { data } = this.props;
+          return(
+            <div className="jumbotron" key={data._id}>
+            <p>{data.question}</p>
+            <p>
+              <label>
+                <input type="radio" name={data._id} value={data.a} onChange={(e) => this.handleChange(e, data)}></input>{data.a}
+              </label>
+              <label>
+              <input type="radio" name={data._id} value={data.b} onChange={(e) => this.handleChange(e, data)}></input>{data.b}
+              </label>
+              <label>
+                <input type="radio" name={data._id} value={data.c} onChange={(e) => this.handleChange(e, data)}></input>{data.c}
+              </label>
+              <label>
+                <input type="radio" name={data._id} value={data.d} onChange={(e) => this.handleChange(e,data)}></input>{data.d}
+              </label>
+            </p>
+          </div>
+          )
+      }
+
+}
+
+export default EachQuestion;
