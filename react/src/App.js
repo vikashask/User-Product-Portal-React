94f6332da3a1e83657726f265aa792bbadf0c185
vikash <vikashraj144@gmail.com>
2019-08-30 00:24:30 +0530

    changes in app

--- react/src/App.js
+++ react/src/App.js
@@ -3,44 +3,41 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import './App.css';
 import Header from "./components/layout/Header";
-import Home from './components/Home';
 import PageNotFound from './components/PageNotFound';
 import Login from './components/Login';
+import Home from './components/Home';
+import User from './components/User';
 import Register from './components/Register';
 import EditUser from './components/EditUser';
 import AddUser from './components/AddUser';
-import User from './components/User';
 import AddProduct from './components/product/AddProduct';
 import EditProduct from './components/product/EditProduct';
 import ViewProduct from './components/product/ViewProduct';
 import Test from './components/test/Test';
 import StartTest from './components/test/StartTest';
-import Authenticate from './components/shared/Authenticate';
+
+import  requireAuth from './utils/requireAuth';
 
 class App extends Component {
   render() {
     return (
       <Router>
           <div>
-              {/* <Header subtitle="React"/> */}
-              {/* <Sidebar/> */}
+              <Header subtitle="React"/>
               <div>
                     <div>
                     <Switch>
                         <Route path='/' component={Login} exact/>
                         <Route path='/register' component={Register} />
-                          <Route path="/" component={Authenticate}>
-                            <Route path='/home' component={Home} exact/>
-                            <Route path='/user' component={User} />
-                            <Route path='/edit-user' component={EditUser} />
-                            <Route path='/add-user' component={AddUser} />
-                            <Route path='/add-product' component={AddProduct} />
-                            <Route path='/edit-product' component={EditProduct} />
-                            <Route path='/view-product' component={ViewProduct} />
-                            <Route path='/test' component={Test} />
-                            <Route path='/start-test' component={StartTest} />
-                        </Route>
-                      
+                        <Route path='/home' component={requireAuth(Home)} exact/>
+                        <Route path='/user' component={requireAuth(User)} />
+                        <Route path='/edit-user' component={EditUser} />
+                        <Route path='/add-user' component={AddUser} />
+                        <Route path='/add-product' component={AddProduct} />
+                        <Route path='/edit-product' component={EditProduct} />
+                        <Route path='/view-product' component={ViewProduct} />
+                        <Route path='/test' component={Test} />
+                        <Route path='/start-test' component={StartTest} /> 
                         <Route component={PageNotFound} />
                     </Switch>
                     </div>
