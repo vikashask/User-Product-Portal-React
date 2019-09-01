c04e5d638c512ebaf4527f258fcf64ce5be0ef72
vikash <vikashraj144@gmail.com>
2019-09-01 22:03:45 +0530

    add question

---
+++ react/src/components/test/AddQuestion.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import TextFieldGroup from '../../utils/TextFieldGroup';
+import PropTypes from 'prop-types';
+
+class AddQuestion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      q_type: '',
+      q_level: '',
+      question: '',
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      e: '',
+      isLoading: false,
+      invalid: false,
+      errors: {},
+      answer:''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.checkUserExists = this.checkUserExists.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  checkUserExists(e) {
+    const field = e.target.name;
+    const val = e.target.value;
+    if (val !== '') {
+      this.props.isUserExists(val).then(res => {
+        let errors = this.state.errors;
+        let invalid;
+        if (res.data.user) {
+          errors[field] = 'There is user with such ' + field;
+          invalid = true;
+        } else {
+          errors[field] = '';
+          invalid = false;
+        }
+        this.setState({ errors, invalid });
+      });
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    // if (this.isValid()) {
+      this.setState({ errors: {}, isLoading: true });
+      this.props.userSignupRequest(this.state).then(
+        () => {
+          this.props.addFlashMessage({
+            type: 'success',
+            text: 'You signed up successfully. Welcome!'
+          });
+          this.context.router.push('/');
+        },
+        (err) => this.setState({ errors: err.response.data, isLoading: false })
+      );
+    // }
+  }
+
+  render() {
+    const { errors } = this.state;
+    // const options = map(timezones, (val, key) =>
+    //   <option key={val} value={val}>{key}</option>
+    // );
+    return (
+      <div className="container">
+      <form onSubmit={this.onSubmit}>
+        <h1>Join our community!</h1>
+
+        <TextFieldGroup
+          error={errors.username}
+          label="Username"
+          onChange={this.onChange}
+          checkUserExists={this.checkUserExists}
+          value={this.state.username}
+          field="username"
+        />
+
+        <TextFieldGroup
+          error={errors.email}
+          label="Email"
+          onChange={this.onChange}
+          checkUserExists={this.checkUserExists}
+          value={this.state.email}
+          field="email"
+        />
+
+
+        {/* <div className={classnames("form-group", { 'has-error': errors.timezone })}>
+          <label className="control-label">Timezone</label>
+          <select
+            className="form-control"
+            name="timezone"
+            onChange={this.onChange}
+            value={this.state.timezone}
+          >
+            <option value="" disabled>Choose Your Timezone</option>
+            {options}
+          </select>
+          {errors.timezone && <span className="help-block">{errors.timezone}</span>}
+        </div> */}
+
+        <div className="form-group">
+          <button disabled={this.state.isLoading || this.state.invalid} className="btn btn-primary btn-lg">
+            Sign up
+          </button>
+        </div>
+      </form>
+      </div>
+    );
+  }
+}
+
+AddQuestion.propTypes = {
+  userSignupRequest: PropTypes.func.isRequired,
+  addFlashMessage: PropTypes.func.isRequired,
+  isUserExists: PropTypes.func.isRequired
+}
+
+AddQuestion.contextTypes = {
+  router: PropTypes.object.isRequired
+}
+
+export default AddQuestion;
