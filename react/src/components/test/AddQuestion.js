4b9e8aeaeb5e8544c66222e99c74bab520d0a2a9
vikash <vikashraj144@gmail.com>
2019-09-08 22:33:44 +0530

    add question

--- react/src/components/test/AddQuestion.js
+++ react/src/components/test/AddQuestion.js
@@ -1,133 +1,154 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Header from "../layout/Header";
 import TextFieldGroup from '../../utils/TextFieldGroup';
-import PropTypes from 'prop-types';
+import * as Constants from '../../utils/Constants';
 
-class AddQuestion extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      q_type: '',
-      q_level: '',
-      question: '',
-      a: '',
-      b: '',
-      c: '',
-      d: '',
-      e: '',
-      isLoading: false,
-      invalid: false,
-      errors: {},
-      answer:''
-    }
-
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.checkUserExists = this.checkUserExists.bind(this);
-  }
-
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  checkUserExists(e) {
-    const field = e.target.name;
-    const val = e.target.value;
-    if (val !== '') {
-      this.props.isUserExists(val).then(res => {
-        let errors = this.state.errors;
-        let invalid;
-        if (res.data.user) {
-          errors[field] = 'There is user with such ' + field;
-          invalid = true;
-        } else {
-          errors[field] = '';
-          invalid = false;
+class AddQuestion extends React.Component { 
+    constructor(prpos){
+        super(prpos);
+        this.state = {
+          q_type: '',
+          q_level: '',
+          question: '',
+          a: '',
+          b: '',
+          c: '',
+          d: '',
+          e: '',
+          isLoading: false,
+          invalid: false,
+          errors: {},
+          subject:'',
+          answer:''
         }
-        this.setState({ errors, invalid });
-      });
+        this.onChange = this.onChange.bind(this);
+        this.onUpdate = this.onUpdate.bind(this);
     }
-  }
 
-  onSubmit(e) {
-    e.preventDefault();
-
-    // if (this.isValid()) {
-      this.setState({ errors: {}, isLoading: true });
-      this.props.userSignupRequest(this.state).then(
-        () => {
-          this.props.addFlashMessage({
-            type: 'success',
-            text: 'You signed up successfully. Welcome!'
-          });
-          this.context.router.push('/');
-        },
-        (err) => this.setState({ errors: err.response.data, isLoading: false })
-      );
-    // }
-  }
-
-  render() {
-    const { errors } = this.state;
-    // const options = map(timezones, (val, key) =>
-    //   <option key={val} value={val}>{key}</option>
-    // );
-    return (
-      <div className="container">
-      <form onSubmit={this.onSubmit}>
-        <h1>Join our community!</h1>
-
-        <TextFieldGroup
-          error={errors.username}
-          label="Username"
-          onChange={this.onChange}
-          checkUserExists={this.checkUserExists}
-          value={this.state.username}
-          field="username"
-        />
-
-        <TextFieldGroup
-          error={errors.email}
-          label="Email"
-          onChange={this.onChange}
-          checkUserExists={this.checkUserExists}
-          value={this.state.email}
-          field="email"
-        />
-
-
-        {/* <div className={classnames("form-group", { 'has-error': errors.timezone })}>
-          <label className="control-label">Timezone</label>
-          <select
-            className="form-control"
-            name="timezone"
-            onChange={this.onChange}
-            value={this.state.timezone}
-          >
-            <option value="" disabled>Choose Your Timezone</option>
-            {options}
-          </select>
-          {errors.timezone && <span className="help-block">{errors.timezone}</span>}
-        </div> */}
-
-        <div className="form-group">
-          <button disabled={this.state.isLoading || this.state.invalid} className="btn btn-primary btn-lg">
-            Sign up
-          </button>
-        </div>
-      </form>
-      </div>
-    );
-  }
+    componentDidMount = () =>{
+        
+    }
+    onChange(e) {
+        console.log("[e.target.name]: e.target.value ",[e.target.name], e.target.value );
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    onUpdate = (event) =>{
+        event.preventDefault();
+        console.log("state value",this.state);
+        fetch(Constants.baseURL + 'question',
+            {
+                method: `POST`,
+                credentials: `include`,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ 'question': this.state.question,
+                    'answer':this.state.answer, 
+                    'q_type': this.state.q_type,
+                    '_id':this.state._id ,
+                    'q_level': this.state.q_level,
+                    'subject': this.state.subject,
+                    'a': this.state.a,
+                    'b': this.state.b,
+                    'c': this.state.c,
+                    'd': this.state.d,
+                    'e': this.state.e,
+                })
+            }).then((res) => {
+                if(res.status === 200) {
+                    res.json().then((response) => {
+                        if(response){
+                            this.props.history.push('/manage-question');
+                        }else{
+                            this.setState({class:'error',errorMsg: 'Unable to update'});
+                        }
+                    })
+                }
+            })
+            .catch((error) => {
+                console.log("error----", error);
+            });
+        // this.props.history.push('/manage-question');
+    }
+    render = () => {
+        return(
+            <div className="container">
+              <Header/>
+                <form className="form-signin">
+                    <h2 className="sub-header">Edit Question : {this.state._id}</h2>
+                    {/* <input type="text" value={this.state.q_type} className="form-control"/> */}
+                    <TextFieldGroup
+                        field="q_type"
+                        value={this.state.q_type}
+                        label="Question Type"
+                        onChange={this.onChange}
+                    />
+                    <TextFieldGroup
+                        field="q_level"
+                        value={this.state.q_level}
+                        label="Question Level"
+                        onChange={this.onChange}
+                    />
+                    <TextFieldGroup
+                        field="subject"
+                        value={this.state.subject}
+                        label="Subject"
+                        onChange={this.onChange}
+                    />
+                    <TextFieldGroup
+                        field="question"
+                        value={this.state.question}
+                        label="Question"
+                        onChange={this.onChange}
+                    />
+                    <TextFieldGroup
+                        field="a"
+                        value={this.state.a}
+                        label="Option A"
+                        onChange={this.onChange}
+                    />
+                    <TextFieldGroup
+                        field="b"
+                        value={this.state.b}
+                        label="Option B"
+                        onChange={this.onChange}
+                    />
+                    <TextFieldGroup
+                        field="c"
+                        value={this.state.c}
+                        label="Option C"
+                        onChange={this.onChange}
+                    />
+                    <TextFieldGroup
+                        field="d"
+                        value={this.state.d}
+                        label="Option D"
+                        onChange={this.onChange}
+                    />
+                    <TextFieldGroup
+                        field="e"
+                        value={this.state.e}
+                        label="Option E"
+                        onChange={this.onChange}
+                    />
+                    <TextFieldGroup
+                        field="answer"
+                        value={this.state.answer}
+                        label="Answer"
+                        onChange={this.onChange}
+                    />
+                    <button onClick={this.onUpdate} className="btn btn-primary" type="submit">Update</button>
+                    <br></br>
+                </form>
+            </div>
+        );
+    }
 }
+const mapStateToProps = (state) =>{
+    return {
+        allData:state.allData
+    }
+};
 
-AddQuestion.propTypes = {
-  userSignupRequest: PropTypes.func.isRequired,
-  addFlashMessage: PropTypes.func.isRequired,
-  isUserExists: PropTypes.func.isRequired
-}
-
-AddQuestion.contextTypes = {
-  router: PropTypes.object.isRequired
-}
-
-export default AddQuestion;
+export default connect(mapStateToProps)(AddQuestion);
